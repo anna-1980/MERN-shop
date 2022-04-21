c1713323d11cab4a5b8c0374a8e15554ba4b052c
Anna Zdyb <anna.zdyb@gmail.com>
2022-04-21 10:09:01 +0200

    introducing router6 and it works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import { Container }  from 'react-bootstrap'
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './components/screens/HomeScreen';
-import { Routes, Route } from 'react-router-dom';
+import ProductScreen from './components/screens/ProductScreen';
 
 const App = () => {
   return (
@@ -15,12 +16,8 @@ const App = () => {
           
           <Routes>
 
-              <Route
-              path='/'
-              element={<HomeScreen />}
-              >
-    
-          </Route>
+          <Route path='/' element= {<HomeScreen />} />
+          <Route path='/product/:id' element= {<ProductScreen />} />
 
         </Routes>
         </Container>
