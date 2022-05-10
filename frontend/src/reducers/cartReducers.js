20d23f262db36933bd888d1d926438fef60bb99c
Anna Zdyb <anna.zdyb@gmail.com>
2022-05-10 16:11:09 +0200

    adding Redux elements and functionality to cart

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,25 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants.js'
+
+export const cartReducer = (state = { cartItems:[]}, action) => {
+    switch(action.type){
+        case CART_ADD_ITEM:
+          const item = action.payload //to check if there is already an item in the cart
+          
+          const existItem = state.cartItems.find(x => x.product === item.product) //that checks if product/items exist and it will have item or false
+        
+          if(existItem) {
+             return{
+                 ...state,   //if item does exist
+                 cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x) // for each x, if x.product id = existItem.product which is the id we will return itterration 
+            } 
+          }else {
+              return {
+                  ...state, 
+                  cartItems:[...state.cartItems, item] //if item doesnt exist
+              }
+          }
+
+          default: 
+            return state
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducer, productDetailsReducer} from './reducers/productReducers' 
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,      // this will be the producsList reducer part of the STATE, you can get that piece of state in components by useSelector 
     productDetails: productDetailsReducer,  //breaking it up into single reducers helps to deal with bugs and fix errors
+    cart: cartReducer,
 });
 
 const initialState = {};
