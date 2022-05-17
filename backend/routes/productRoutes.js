122505976f02214423d068f73e71e33a7169a062
Anna Zdyb <anna.zdyb@gmail.com>
2022-05-17 16:26:18 +0200

    create userAuth routes and generate TOKEN

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import {
+  getProducts, 
+  getProductById
+} from '../controllers/productController.js' 
+ 
 
 const router = express.Router()
 
@@ -8,29 +11,10 @@ const router = express.Router()
 // @route GET/api/products
 // @access public
 
-router.get('/', asyncHandler( async (req, res) => {
-
-  const products = await Product.find({})
-
-  res.json(products)
-}))
-
-// description Fetch single product 
-// @route GET/api/products/:id
-// @access public
-
-router.get('/:id', asyncHandler( async (req, res) => {
-  const product =  await Product.findById(req.params.id)
-
-  if (product){
-    res.json(product)
-  }else{
-    res.status(404)
-    throw new Error('Product not found')
-  }
-
-  console.log(req.params.id);
-  
-}))
+// router.get('/', 
+   
+//  )
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById)
 
 export default router
