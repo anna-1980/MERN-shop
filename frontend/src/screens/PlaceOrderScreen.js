53df6bc47e09a3d5d2a80e97e32765a21ad9bc2f
Anna Zdyb <anna.zdyb@gmail.com>
2022-06-13 20:32:02 +0200

    adding CheckoutScreen and fixing it's layout, 8 Col + 4 Col

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -8,12 +8,27 @@ import { saveShippingAddress } from '../actions/cartActions.js';
 
 const PlaceOrderScreen = () => {
     const cart = useSelector(state => state.cart)
-    console.log(cart.cartItems)
+    // console.log(cart.cartItems)
+
+    // make sure 2 decimal places are showign at all times
+   const addDecimals = (num) => {
+       return (Math.round(num *100)/ 100).toFixed(2)
+   }
+    //calculate Prices:
+    cart.itemsPrice = addDecimals(cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2))
+    cart.shippingPrice = addDecimals(cart.itemsPrice > 50 ? 0 : 50) 
+    cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+    cart.TotalPrice = addDecimals((Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2))
+
+    const placeOrderHandler = () => {
+        console.log('you have placed the order')
+    }
+
   return (
    <>
    <CheckoutSteps step1 step2 step3 step4/>
    <Row>
-       <Col md={8}>
+       <Col md={8} className="m-auto">
            <ListGroup variant='flush'>
                <ListGroup.Item>
                    <h2>Shipping</h2>
@@ -37,10 +52,11 @@ const PlaceOrderScreen = () => {
                     <Message> Your Cart Is Empty</Message>
                 : (
                     <ListGroup variant ="flush">
-                        {cart.cartItems.map((item, index) => ( 
+                        {
+                        cart.cartItems.map((item, index) => ( 
                             <ListGroup.Item key={index}>
-                                <Row md={1}>
-                                    <Col>
+                                <Row >
+                                    <Col md={1}>
                                         <Image src={item.image} alt={item.name}
                                         fluid
                                         rouded
@@ -51,6 +67,14 @@ const PlaceOrderScreen = () => {
                                             {item.name}
                                         </Link>
                                     </Col>
+                                    <Col md={4}>
+                                        
+                        {/* calculate the total of the order */}
+                                        
+                                        {item.qty} x {item.price} =  {(item.qty * item.price).toFixed(2)} 
+
+                                    </Col>
+                                 
                                 </Row>
                             </ListGroup.Item>
                         ))}
@@ -59,6 +83,46 @@ const PlaceOrderScreen = () => {
                </ListGroup.Item>
            </ListGroup>
        </Col>
+       <Col md={4}>
+            <Card md={4} sm={4}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>Order Summary</h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Items:</Col>
+                            <Col>{cart.itemsPrice}</Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Shipping:</Col>
+                            <Col>{cart.shippingPrice}</Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Tax:</Col>
+                            <Col>{cart.taxPrice}</Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Total Price:</Col>
+                            <Col>cart TotalPrice</Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                            <Button 
+                            type='button'
+                            className='btn=block'
+                            disabled={cart.cartItems ===0}
+                            onClick={placeOrderHandler}>Place Order</Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </Col>
    </Row>
    </>
   )
