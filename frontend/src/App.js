13e21a057fab3727293d76741d4d0962abc92e4f
Anna Zdyb <anna.zdyb@gmail.com>
2022-06-22 14:54:43 +0200

    adding ProductEditScreen so Admin can edit product information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+ 
 
 const App = () => {
   return (
@@ -39,7 +41,7 @@ const App = () => {
             <Route path='/cart/*' element= {<CartScreen />} />
             <Route path='/admin/userlist' element={<UserListScreen /> }/>
             <Route path='/admin/productlist' element={<ProductListScreen /> }/>
-            <Route path='/admin/user/:id/edit' element={<UserEditScreen /> }/>
+            <Route path='/admin/user/:id/edit' element={<ProductEditScreen /> }/>
           </Routes>
         </Container>
       </main>
