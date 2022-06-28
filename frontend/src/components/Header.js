bfb34e39197f0f25d2e0db6121d78eab3f51d48c
Anna Zdyb <anna.zdyb@gmail.com>
2022-06-28 11:01:48 +0200

    after deploying to Heroku

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const  Header = () => {
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Biology Bag Shop</Navbar.Brand>
+            <Navbar.Brand>Demo: Biology Bag Shop</Navbar.Brand>
           </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
